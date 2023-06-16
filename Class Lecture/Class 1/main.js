2a5c13b5e1c46e2a5270ea9ea7a8f1e88faa5360
saiedrahman <saiedtechit@gmail.com>
2023-06-16 12:07:59 +0600

    Add basic script of JS

---
+++ Class Lecture/Class 1/main.js
@@ -0,0 +1,33 @@
+// Declaring variables and constant
+
+let firstName = "Md Saiedur Rahman";
+let lastName = "Saied";
+const id = '222015**'
+
+// print variables and constant 
+
+console.log(firstName, lastName);
+console.log(id);
+
+// Declare Array
+
+let randomThings = [1 , 2, 3, 5, 6, 2234, '0384u'];
+
+// print array 
+
+console.log(randomThings);
+
+// add data on Array
+
+randomThings.push("Saied");
+
+// delete data from Array 
+
+randomThings.pop();
+
+// remove specific data from Array 
+
+randomThings.splice(3, 1);
+console.log(randomThings);
+
+
